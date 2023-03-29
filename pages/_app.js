f1a695cb9102d0ad05b518eff79ba5878962595b
CrypticzXI <CrypticzXI@gmail.com>
2023-03-29 01:11:19 +0100

    Make font go brrr

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,20 @@
 import '../styles/globals.css'
+import Head from 'next/head'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <div> 
+      <Head>
+        {/* preconnect scripts... */}
+        <link
+          href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Rubik:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
+
+      <Component {...pageProps} />
+    </div>
+  )
 }
 
 export default MyApp
